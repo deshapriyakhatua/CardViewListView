468ba40107c4f04ac2cbf44a9eb1fa9a5125876b
deshapriyakhatua <deshapriyakhatua@gmail.com>
2022-10-11 20:09:30 +0530

    11 10 2022

--- app/src/main/java/com/example/listviewcardview/MainActivity.java
+++ app/src/main/java/com/example/listviewcardview/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.listviewcardview;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -19,18 +23,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] arr = new String[]{"30","20","40","30","20","10"};
+
+        CustomAdapter adapter = new CustomAdapter(this,R.layout.card_view,arr);
 
         listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
 
     }
-    public class customAdapter extends ArrayAdapter<String>{
+    public class CustomAdapter extends ArrayAdapter<String>{
 
 
-        public customAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
+        public CustomAdapter(@NonNull Context context, int resource, @NonNull String[] objects) {
             super(context, resource, objects);
         }
-        public View getView(){
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
 
+            LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+            convertView = layoutInflater.inflate(R.layout.card_view, parent, false);
+
+             String str = getItem(position);
+             TextView tv = convertView.findViewById(R.id.textView);
+             tv.setText(str+"% OFF");
+             return convertView;
         }
     }
 }
